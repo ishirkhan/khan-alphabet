1c21bd64302b0b8ced250f8d301266e2429f5f33
babbage <601728820@qq.com>
2022-03-10 15:04:41 +0800

    feat: 构建参数和位置调整

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 const path = require("path");
 const { defineConfig } = require("vite");
+const packageName = require("./package.json").name;
 
 module.exports = defineConfig({
   build: {
     minify: true,
-    outDir: "dist",
+    outDir: "dist/lib",
     sourcemap: false,
     lib: {
-      name: "khan-table",
+      name: packageName,
       entry: path.resolve(__dirname, "src/lib/index.ts"),
-      fileName: (format) => `khan-table.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {},
   },
