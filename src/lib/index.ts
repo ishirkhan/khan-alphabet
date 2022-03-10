d338f9c55cf347692d13b686bcca6b9845026418
babbage <601728820@qq.com>
2022-03-10 17:24:05 +0800

    fix: 因remark的导出导致的vscode等node环境报document.body找不到错误临时屏蔽

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 export * from "./alphabet";
 export * from "./retext";
-export * from "./remark";
+// export * from "./remark"; // 因remark 的编译导致 document.body 未找到错误，目前还没找到正确打包的方式，所以功能需要的地方直接复制，不做编译打包了。所以这里先从包中去掉它了。
